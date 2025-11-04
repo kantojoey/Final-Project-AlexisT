75d82327338818be81fd7f68fb7643135f128ead
Alexis <atinoco999@gmail.com>
2025-11-03 19:18:01 -0600

    Set up API access token request in App.jsx

--- hit-record/src/App.jsx
+++ hit-record/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 // API User ID and Key for access
@@ -7,7 +7,28 @@ const clientID = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
-  console.log("Testing")
+
+  const [accessToken, setAccessToken] = useState();
+  
+  useEffect(()=> {
+    let authCredentials = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body:
+        "grant_type=client_credentials&client_id="+clientID+"&client_secret="+clientSecret,
+    };
+
+    // Fetch returns access token object with: access_token, token_type, and expires_in
+    fetch("https://accounts.spotify.com/api/token", authCredentials)
+      .then((response) => response.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      }
+    );
+  }, []);
+
 
   return (
     <div>
---
+++ hit-record/src/components/pages/ListeningLogPage.jsx
@@ -0,0 +1,9 @@
+const ListeningLogPage = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default ListeningLogPage;
